59b704a09235e68c40bd82722880e25968ce5298
sert <demo.sert@gmail.com>
2016-06-08 15:32:58 +0900

    add decrement method

--- main.c
+++ main.c
@@ -4,8 +4,14 @@ int inc(int x) {
   return x + 1;
 }
 
+int dec(int x) {
+  return x - 1;
+}
+
 int main() {
-  int result = inc(5);
+  int inc_result = inc(5);
+  int dec_result = dec(5);
   printf("Hello world\n");
-  printf("result = %d\n", result);
+  printf("inc_result = %d\n", inc_result);
+  printf("dec_result = %d\n", dec_result);
 }
